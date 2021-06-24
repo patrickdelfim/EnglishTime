6ee461798d29c1dbab87ea9972d30d9c463f018c
patrick delfim <55807714+patrickdelfim@users.noreply.github.com>
2021-06-24 18:54:37 -0300

    Delete semana5 directory

--- semana5/II_Ibovespa.java
+++
@@ -1,9 +0,0 @@
-import java.util.ArrayList;
-
-public interface II_Ibovespa {
-
-    public static final float PI = 3.14f;
-
-    public void listar(ArrayList<Acao> acoes);
-    public void ordenar(ArrayList<Acao> acoes);
-}
